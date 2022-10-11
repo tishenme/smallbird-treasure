4bf42ebe69c68ac963933a17fd90b950ca61d26a
tishenme@163.com <152433Xx>
2022-10-11 20:01:49 +0800

    tishen update deploy pages to gitee and github

--- deploy-site.sh
+++ deploy-site.sh
@@ -3,16 +3,30 @@
 # 生成静态文件
 yarn docs:build
 
-# 发布静态文件到 Gitee Page 需要配置好 Gitee SSH 密钥
-echo -e "\n###### Deploy Gitee Page ######\n"
+# 基本步骤
 cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@gitee.com:tishenme/smallbird-treasure-site.git master:master
-cd ..
-rm -rf dist
 
-# 发布地址
+# 发布静态文件到 Gitee Page 需要配置好 Gitee SSH 密钥
+echo -e "\n###### Deploy Gitee Page ######\n"
+git push -f git@gitee.com:tishenme/smallbird-treasure-site.git master:master
+
+# 发布地址 Gitee 需要到去 pages 页面刷新
 # https://gitee.com/tishenme/smallbird-treasure-site/pages
 # https://gitee.com/tishenme/smallbird-treasure-site
+# https://tishenme.gitee.io/smallbird-treasure-site/
+
+# 发布静态文件到 Github Page 需要配置好 Github SSH 密钥
+echo -e "\n###### Deploy Github Page ######\n"
+git push -f git@github.com:tishenme/smallbird-treasure-site.git master:master
+
+# 发布地址 Github 不需要到去 pages 页面刷新
+# https://github.com/tishenme/smallbird-treasure-site/pages
+# https://github.com/tishenme/smallbird-treasure-site
+# https://tishenme.github.io/smallbird-treasure-site/
+
+# 删除文件
+cd ..
+rm -rf dist
