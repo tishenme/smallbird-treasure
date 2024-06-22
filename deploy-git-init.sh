2e71a9773fd5e00095f1c8db5d608d98d71a7cfd
unknown <tishenme@163.com>
2024-06-22 18:58:12 +0800

    tishen update refactor

--- deploy-git-init.sh
+++ deploy-git-init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+# #!/bin/sh
 
 # sh deploy-git-init.sh
 
--- deploy-git-update.sh
+++ deploy-git-update.sh
@@ -10,10 +10,10 @@ git commit -m "tishen update refactor"
 # cd ~
 # ssh-keygen -t rsa -C "tishenme@163.com"
 
-# 需要配置好 Gitee SSH 密钥
-# https://gitee.com/tishenme/smallbird-treasure
-echo -e "\n###### Push Gitee ######\n"
-git push git@gitee.com:tishenme/smallbird-treasure.git master:master
+# # 需要配置好 Gitee SSH 密钥
+# # https://gitee.com/tishenme/smallbird-treasure
+# echo -e "\n###### Push Gitee ######\n"
+# git push git@gitee.com:tishenme/smallbird-treasure.git master:master
 
 # 需要配置好 Github SSH 密钥
 # https://github.com/tishenme/smallbird-treasure
